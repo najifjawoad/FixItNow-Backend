eac8085c8de2ef133c7118410b9af106cec44ad5
najif jawoad jim <najifjawoad@gmail.com>
2026-07-17 16:18:14 +0600

    project steup done and dependencies are added

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+
+
+const PORT = config.port || 3000;
+async function main() {
+  try {
+    // await prisma.$connect();
+    console.log("Connected to the DB successfully");
+
+    app.listen(PORT, () => {
+      console.log(`FixItNow Server Is Running on Port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error Starting the server", Error);
+    // await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
