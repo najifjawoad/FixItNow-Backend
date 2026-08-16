a588aa9ef7b2f7a2bfb7dce2ad785e0e4929de94
najif jawoad jim <najifjawoad@gmail.com>
2026-08-17 02:19:07 +0600

    fix: repair syntax formatting in technician.route.ts

--- src/modules/technician/technician.route.ts
+++ src/modules/technician/technician.route.ts
@@ -6,21 +6,24 @@ import { Role } from "../../../generated/prisma/enums";
 const router = Router();
 
 // create services :
-router.post("/services" ,auth(Role.TECHNICIAN) ,technicianController.createServices );
+router.post("/services", auth(Role.TECHNICIAN), technicianController.createServices);
 
 // create availability
-router.post("/availability", auth(Role.TECHNICIAN), technicianController.createAvailability)
+router.post("/availability", auth(Role.TECHNICIAN), technicianController.createAvailability);
 
 // Get all categories :
-router.get("/allCategories" , auth(Role.ADMIN , Role.TECHNICIAN)  , technicianController.getAllCategories);
+router.get("/allCategories", auth(Role.ADMIN, Role.TECHNICIAN), technicianController.getAllCategories);
 
 // update availability :
-router.patch("/update-availability" , auth(Role.TECHNICIAN) , technicianController.updateAvailability)
+router.patch("/update-availability", auth(Role.TECHNICIAN), technicianController.updateAvailability);
 
 // update users booking status :
 router.patch(
   "/bookings/status/:bookingId",
   auth(Role.TECHNICIAN),
+  technicianController.updateUsersBookingStatus
+);
+
 // get my services:
 router.get("/my-services", auth(Role.TECHNICIAN), technicianController.getMyServices);
 
